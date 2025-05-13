cf8f31de3749aa13fd2a27bcd3086336125979a8
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-05-13 19:00:53 +0200

    add cool view

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::io::{Stdout, stdout};
+
+use crossterm::{
+    queue,
+    style::{
+        Attribute, Attributes, Color, SetAttribute, SetAttributes, SetBackgroundColor,
+        SetForegroundColor,
+    },
+};
 use html_parser::{Dom, Node};
 use reqwest::{Client, Response, Url};
 
 const ERROR_PAGE_BODY: &str = include_str!("error.html");
+const HOME_PAGE_BODY: &str = include_str!("home.html");
 
 async fn parse_html_request(request: Result<Response, reqwest::Error>) -> Dom {
     if let Ok(response) = request {
@@ -27,21 +37,129 @@ impl Webpage {
             url: Some(url),
         }
     }
+    fn from_str(body_text: &str) -> Self {
+        let body = Dom::parse(body_text)
+            .unwrap_or(Dom::parse(ERROR_PAGE_BODY).expect("error page should be valid html"));
+        Self { body, url: None }
+    }
 }
 
 const IGNORE_ELEMENTS: &[&str] = &["style", "script", "title"];
 
-fn get_viewable_elements(items: &Vec<Node>, buf: &mut Vec<String>) {
+fn trim_repeated_whitespace(s: &str) -> String {
+    let words: Vec<_> = s.split_whitespace().collect();
+    let mut text = words.join(" ");
+    text
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct RenderState {
+    respect_whitespace: bool,
+    foreground_color: Color,
+    background_color: Color,
+    bold: bool,
+    italics: bool,
+}
+impl Default for RenderState {
+    fn default() -> Self {
+        Self {
+            respect_whitespace: false,
+            foreground_color: Color::White,
+            background_color: Color::Reset,
+            bold: false,
+            italics: false,
+        }
+    }
+}
+impl RenderState {
+    /// Uses crossterm to apply styles to console output.
+    /// Used for color, italics, bold text, etc
+    fn format_terminal(&self) {
+        queue!(stdout(), SetAttribute(Attribute::Reset)).unwrap();
+        queue!(stdout(), SetForegroundColor(self.foreground_color)).unwrap();
+        queue!(stdout(), SetBackgroundColor(self.background_color)).unwrap();
+        if self.bold {
+            queue!(stdout(), SetAttribute(Attribute::Bold)).unwrap();
+        }
+        if self.italics {
+            queue!(stdout(), SetAttribute(Attribute::Italic)).unwrap();
+        }
+    }
+}
+
+fn render_element(
+    items: &Vec<Node>,
+    buf: &mut Vec<String>,
+    recursion_level: usize,
+    render_state: RenderState,
+    mut ended_with_newline: bool,
+) {
     for item in items {
         match item {
             Node::Text(text) => {
-                buf.push(text.clone());
+                let mut text = text.clone();
+                if !render_state.respect_whitespace {
+                    text = text.replace("\n", "");
+                    text = text.replace("\r", "");
+                    text = strip_ansi_escapes::strip_str(text);
+                    text = trim_repeated_whitespace(&text);
+                }
+                text = String::from(" ") + &text;
+                print!("{}", text);
+                ended_with_newline = false;
             }
             Node::Element(element) => {
+                //println!("{} {} {:?}", element.name, recursion_level, render_state);
+                let mut new_render_state = render_state.clone();
                 if IGNORE_ELEMENTS.contains(&element.name.as_str()) {
                     continue;
                 }
-                get_viewable_elements(&element.children, buf);
+                let element_needs_linebreak =
+                    ["p", "pre"].contains(&element.name.as_str()) || element.name.starts_with("h");
+
+                if element_needs_linebreak && !ended_with_newline {
+                    println!();
+                }
+
+                match element.name.as_str() {
+                    "pre" => {
+                        new_render_state.respect_whitespace = true;
+                        new_render_state.background_color = Color::Green;
+                    }
+                    "em" => {
+                        new_render_state.italics = true;
+                    }
+                    _ => {
+                        if element.name.starts_with("h") && element.name.len() == 2 {
+                            new_render_state.foreground_color = Color::Red
+                        }
+                    }
+                }
+
+                // update console style to new render state
+                // this will ex. make <em> tags have their text printed with italics
+                // and make <a> links be highlighted
+                if new_render_state != render_state {
+                    new_render_state.format_terminal();
+                }
+
+                render_element(
+                    &element.children,
+                    buf,
+                    recursion_level + 1,
+                    new_render_state,
+                    ended_with_newline,
+                );
+
+                // restore old render state
+                // i.e. the parent elements style
+                if new_render_state != render_state {
+                    render_state.format_terminal();
+                }
+                ended_with_newline = element_needs_linebreak;
+                if element_needs_linebreak {
+                    println!();
+                }
             }
             _ => {}
         }
@@ -55,13 +173,7 @@ struct Pop {
 impl Pop {
     async fn new() -> Self {
         let client = Client::new();
-        let history = vec![
-            Webpage::from_url(
-                Url::parse("https://example.com").expect("default url should be valid"),
-                &client,
-            )
-            .await,
-        ];
+        let history = vec![Webpage::from_str(HOME_PAGE_BODY)];
         Self { client, history }
     }
     fn get_current_page(&self) -> &Webpage {
@@ -70,7 +182,13 @@ impl Pop {
     fn draw(&self) {
         let current_page = self.get_current_page();
         let mut all_elements = Vec::new();
-        get_viewable_elements(&current_page.body.children, &mut all_elements);
+        render_element(
+            &current_page.body.children,
+            &mut all_elements,
+            0,
+            RenderState::default(),
+            false,
+        );
         for element in all_elements {
             println!("{element}");
         }
